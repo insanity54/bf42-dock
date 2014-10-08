db188d57d2e4b77f844d21c35614bf83bdafa596
Chris Grimmett <chris@grimtech.net>
2014-10-07 21:49:41 -0700

    made start script more verbose

--- assets/start.sh
+++ assets/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # send heartbeat to the new master server (workaround for gamespy shutdown)
+echo 'starting battlefield 1942 server'
 echo '109.71.69.254 master.gamespy.com' >> /etc/hosts
 
 # start the server
